3ace22701a15ee234c6d909d23931d66df336124
Huỳnh Thế Nghĩa <0306241130@caothang.edu.vn>
2026-07-14 18:35:37 +0700

    sua ListProduct

--- techstore/src/components/ProductList.jsx
+++ techstore/src/components/ProductList.jsx
@@ -2,16 +2,16 @@ import products from "../data/product";
 import ProductCard from "./ProductCard";
 
 export default function ProductList() {
-    return (
-        <>
-            {products.map((product) => {
-                <ProductCard
-                    id={product.id}
-                    name={product.name}
-                    price={product.price}
-                    cartogery={product.cartogery}
-                />
-            })}
-        </>
-    );
+  return (
+    <>
+      {products.map((product) => (
+        <ProductCard
+          id={product.id}
+          name={product.name}
+          price={product.price}
+          cartogery={product.cartogery}
+        />
+      ))}
+    </>
+  );
 }
--- techstore/src/data/product.js
+++ techstore/src/data/product.js
@@ -1,28 +1,28 @@
-const products= [
-    {
-        id: 1,
-        name : "Mackbook AIR M4",
-        catogery : "Apple",
-        price : 289990000
-    },
-    {
-        id: 2,
-        name : "Mackbook AIR M4",
-        catogery : "Apple",
-        price : 289990000
-    },
-    {
-        id: 3,
-        name : "Mackbook AIR M4",
-        catogery : "Apple",
-        price : 289990000
-    },
-    {
-        id: 4,
-        name : "Mackbook Air M4",
-        cartogery : "Apple",
-        price : 289990000
-    },
-]
+const products = [
+  {
+    id: 1,
+    name: "Mackbook AIR M4",
+    catogery: "Apple",
+    price: 289990000,
+  },
+  {
+    id: 2,
+    name: "IPhone 14Pro",
+    catogery: "Apple",
+    price: 399990000,
+  },
+  {
+    id: 3,
+    name: "ASUS TUF15",
+    catogery: "ASUS",
+    price: 389990000,
+  },
+  {
+    id: 4,
+    name: "Mackbook Air M2",
+    cartogery: "Apple",
+    price: 209990000,
+  },
+];
 
 export default products;
